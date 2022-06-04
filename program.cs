65a9250b0b73d91c496cc523d4f745688f614dce
KadenCodes <KadenCode@gmail.com>
2022-06-04 19:07:26 -0400

    Fixed Knowledge Error

--- program.cs
+++ program.cs
@@ -1,19 +1,6 @@
 ﻿namespace product
 {
-    public class prod2
-    {
-        public string name = "rag";
-        string price = "19.99";
-        int id = 2;
-        int oh = 12;
-        public void info()
-        {
-            Console.WriteLine($"Price: {price}");
-            Console.WriteLine($"Id: {id}");
-            Console.WriteLine($"Ammount on hand: {oh}");
-        }
-    }
-    public class prod1
+    public class product
     {
         public string name = "towel";
         public string price = "10.99";
@@ -27,8 +14,8 @@
         }
         public static void Main(string[] args)
         {
-            prod2 rag = new prod2();
-            prod1 towel = new prod1();
+            product rag = new product();
+            product towel = new product();
             Console.WriteLine(towel.name);
             towel.info();
             Console.WriteLine(rag.name);
