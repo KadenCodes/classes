b82e428c7a998ab8f801098644eaddf7ab779e1d
KadenCodes <KadenCode@gmail.com>
2022-06-04 18:51:19 -0400

    first commit

---
+++ program.cs
@@ -0,0 +1,39 @@
+﻿namespace product
+{
+    public class prod2
+    {
+        public string name = "rag";
+        string price = "19.99";
+        int id = 2;
+        int oh = 12;
+        public void info()
+        {
+            Console.WriteLine($"Price: {price}");
+            Console.WriteLine($"Id: {id}");
+            Console.WriteLine($"Ammount on hand: {oh}");
+        }
+    }
+    public class prod1
+    {
+        public string name = "towel";
+        public string price = "10.99";
+        public int id = 1;
+        public int oh = 11;
+        public void info()
+        {
+            Console.WriteLine($"Price: {price}");
+            Console.WriteLine($"Id: {id}");
+            Console.WriteLine($"Ammount on hand: {oh}");
+        }
+        public static void Main(string[] args)
+        {
+            prod2 rag = new prod2();
+            prod1 towel = new prod1();
+            Console.WriteLine(towel.name);
+            towel.info();
+            Console.WriteLine(rag.name);
+            rag.info();
+
+        }
+    }
+}
